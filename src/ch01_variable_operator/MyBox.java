04046527b0a4395aaab832343d2bd9c3d2fca4d7
lcg54 <rjscodla98@naver.com>
2025-07-23 18:22:05 +0900

    커ㅓㅓ밋

--- src/ch01_variable_operator/MyBox.java
+++ src/ch01_variable_operator/MyBox.java
@@ -6,6 +6,6 @@ public class MyBox {
         double x = Math.sqrt(Math.pow(w,2)+Math.pow(h,2)); // 루트함수, 제곱함수
 
         System.out.println("사각형의 넓이 : " + w*h + "\n사각형의 둘레 : " + (w+h)*2);
-        System.out.println("대각선의 길이 : " + x);
+        System.out.print("대각선의 길이 : " + Math.round(x)); // 반올림함수
     }
 }
