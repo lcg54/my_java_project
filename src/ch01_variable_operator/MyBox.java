4edd1a58fabfd4a9b1c1119e3012ac8fdb02d15f
lcg54 <rjscodla98@naver.com>
2025-07-23 18:04:40 +0900

    변수와 연산자

--- src/ch01_variable_operator/MyBox.java
+++ src/ch01_variable_operator/MyBox.java
@@ -3,6 +3,9 @@ package ch01_variable_operator;
 public class MyBox {
     public static void main(String[] args) {
         double w = 10.0, h = 5.0;
+        double x = Math.sqrt(Math.pow(w,2)+Math.pow(h,2)); // 루트함수, 제곱함수
+
         System.out.println("사각형의 넓이 : " + w*h + "\n사각형의 둘레 : " + (w+h)*2);
+        System.out.println("대각선의 길이 : " + x);
     }
 }
