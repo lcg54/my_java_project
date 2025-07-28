6653112ae20b94473f59af30bbf546048514294d
lcg54 <rjscodla98@naver.com>
2025-07-28 17:51:40 +0900

    for

---
+++ src/ch02_control_statement/ForInFor.java
@@ -0,0 +1,14 @@
+package ch02_control_statement;
+
+public class ForInFor {
+    public static void main(String[] args) {
+        System.out.println("[ 구구단 표 ]");
+        for (int i = 2; i <= 9; i++) {
+            System.out.print(+ i + "단 :  ");
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i + " * " + j + " = " + i*j + "   ");
+            }
+            System.out.println();
+        }
+    }
+}
