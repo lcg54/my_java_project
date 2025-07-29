ece69dc76acc12b02d6b15e05673eee7ab99b039
lcg54 <rjscodla98@naver.com>
2025-07-29 13:11:57 +0900

    for 중첩

--- src/ch02_control_statement/Gugudan01.java
+++ src/ch02_control_statement/Gugudan01.java
@@ -1,12 +1,12 @@
 package ch02_control_statement;
 
-public class ForInFor {
+public class Gugudan01 {
     public static void main(String[] args) {
-        System.out.println("\n[ 구구단 표 ]");
+        System.out.println("[ 구구단 표 ]");
         for (int i = 2; i <= 9; i++) {
-            System.out.print(+ i + "단 :  ");
+            System.out.print(i + "단 :\t");
             for (int j = 1; j <= 9; j++) {
-                System.out.print(i + " * " + j + " = " + i*j + "   ");
+                System.out.print(i + " * " + j + " = " + i*j + "\t");
             }
             System.out.println();
         }
