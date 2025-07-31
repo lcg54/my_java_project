058df6ab16e12ef2b14fe7cc14fa76c2a90223b6
lcg54 <rjscodla98@naver.com>
2025-07-31 16:59:31 +0900

    method overloading

--- src/ch04_class/ShapeArea.java
+++ src/ch04_class/ShapeArea.java
@@ -3,13 +3,13 @@ package ch04_class;
 public class ShapeArea {
     final double PI = 3.141593;
 
-    public void area(int a) {
+     void area(int a) {
         System.out.println(a*a*PI);
     }
-    public void area(int a, int b) {
+     void area(int a, int b) {
         System.out.println(a*b);
     }
-    public void area(int a,int b,int c) {
+     void area(int a,int b,int c) {
         System.out.println((a+b)*c/2.0);
     }
 }
